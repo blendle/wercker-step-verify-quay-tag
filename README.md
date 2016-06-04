cc7c3f80a26c9b04d735045e0c75b560228eef75
Jean Mertz <jean@mertz.fm>
2016-06-04 22:40:42 +0200

    set tag to $WERCKER_GIT_COMMIT by default

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ deploy:
   - blendle/verify-quay-tag:
     token: hello-world          # optional, for private repositories
     repository: blendle/true    # required, org/repo format
-    tag: other                  # optional, defaults to "latest"
+    tag: other                  # optional, defaults to "$WERCKER_GIT_COMMIT" or "latest"
     message: 4-oh-4!            # optional, printed failure message
 ```
 
--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 main() {
+  tag="$WERCKER_VERIFY_QUAY_TAG_TAG"
+
+  if [ -z "$tag" ]; then
+    tag="${WERCKER_GIT_COMMIT:-latest}"
+  fi
+
   if [ -n "$WERCKER_VERIFY_QUAY_TAG_TOKEN" ]; then
     auth="--header 'Authorization: Bearer $WERCKER_VERIFY_QUAY_TAG_TOKEN'"
   fi
 
-  curl_with_flags "$auth" "https://quay.io/api/v1/repository/$WERCKER_VERIFY_QUAY_TAG_REPOSITORY/tag/$WERCKER_VERIFY_QUAY_TAG_TAG/images" \
+  curl_with_flags "$auth" "https://quay.io/api/v1/repository/$WERCKER_VERIFY_QUAY_TAG_REPOSITORY/tag/$tag/images" \
     || fail "$WERCKER_VERIFY_QUAY_TAG_MESSAGE"
 }
 
