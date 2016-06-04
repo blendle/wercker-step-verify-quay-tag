f877a50b5541506e9562102e030363b4da48b5f0
Jean Mertz <jean@mertz.fm>
2016-06-04 22:28:44 +0200

    run.sh tweaks

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 main() {
-  local args
-
-  args="--fail --silent --output /dev/null"
-
   if [ -n "$WERCKER_VERIFY_QUAY_TAG_TOKEN" ]; then
-    args="$args --header 'Authorization: Bearer $WERCKER_VERIFY_QUAY_TAG_TOKEN'"
+    auth="--header 'Authorization: Bearer $WERCKER_VERIFY_QUAY_TAG_TOKEN'"
   fi
 
-  curl $args https://quay.io/api/v1/repository/$WERCKER_VERIFY_QUAY_TAG_REPOSITORY/tag/$WERCKER_VERIFY_QUAY_TAG_TAG/images \
-    || fail $WERCKER_VERIFY_QUAY_TAG_MESSAGE
+  curl_with_flags "$auth" "https://quay.io/api/v1/repository/$WERCKER_VERIFY_QUAY_TAG_REPOSITORY/tag/$WERCKER_VERIFY_QUAY_TAG_TAG/images" \
+    || fail "$WERCKER_VERIFY_QUAY_TAG_MESSAGE"
 }
+
+curl_with_flags() { curl --fail --silent --output /dev/null "$@"; }
 
 main
